4bae5465abf1e4b69fe7fda27e29bac9348a1b90
nuzair46 <nuzer@REDNEK46>
2022-06-12 21:03:52 +0530

    risky functions everywhere

--- ReModCE/Managers/RiskyFunctionsManager.cs
+++ ReModCE/Managers/RiskyFunctionsManager.cs
@@ -6,7 +6,6 @@ using MelonLoader;
 using ReMod.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using VRC.Core;
 
 namespace ReModCE.Managers
 {
@@ -31,7 +30,7 @@ namespace ReModCE.Managers
         {
             Instance = this;
         }
-        
+
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
             if (buildIndex == -1) // custom scene
@@ -43,40 +42,8 @@ namespace ReModCE.Managers
         private IEnumerator CheckWorld()
         {
             while (RoomManager.field_Internal_Static_ApiWorld_0 == null) yield return new WaitForEndOfFrame();
-            
-            var apiWorld = RoomManager.field_Internal_Static_ApiWorld_0;
-            var apiWorldInstance = RoomManager.field_Internal_Static_ApiWorldInstance_0;
 
-            bool riskyFunctionAllowed;
-            if (apiWorldInstance.type <= InstanceAccessType.FriendsOfGuests)
-            {
-                var worldName = apiWorld.name.ToLower();
-                var tags = new List<string>();
-                foreach (var tag in apiWorld.tags)
-                {
-                    tags.Add(tag.ToLower());
-                }
-
-                var hasBlacklistedTag = _blacklistedTags.Any(tag => tags.Contains(tag));
-                riskyFunctionAllowed =
-                    !worldName.Contains("club") && !worldName.Contains("game") && !hasBlacklistedTag;
-
-                var rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
-                if (rootGameObjects.Any(go => go.name is "eVRCRiskFuncDisable" or "UniversalRiskyFuncDisable"))
-                {
-                    riskyFunctionAllowed = false;
-                }
-                else if (rootGameObjects.Any(go => go.name is "eVRCRiskFuncEnable" or "UniversalRiskyFuncEnable"))
-                {
-                    riskyFunctionAllowed = true;
-                }
-            }
-            else
-            {
-                riskyFunctionAllowed = true;
-            }
-
-            RiskyFunctionAllowed = riskyFunctionAllowed;
+            RiskyFunctionAllowed = true;
             OnRiskyFunctionsChanged?.Invoke(RiskyFunctionAllowed);
         }
     }
